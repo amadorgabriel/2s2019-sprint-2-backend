723ef110fae999763e27eb597052e0e5c9af389d
Gabriel Rodrigues Amador <44680781890@info.local>
2019-08-20 17:35:46 -0300

    up

--- Sstop - API/Senai.Sstop.WebApi/Senai.Sstop.WebApi/Controllers/EstilosController.cs
+++ Sstop - API/Senai.Sstop.WebApi/Senai.Sstop.WebApi/Controllers/EstilosController.cs
@@ -9,29 +9,25 @@ using Senai.Sstop.WebApi.Repositories;
 
 namespace Senai.Sstop.WebApi.Controllers
 {
+    // RESULTADO:
+    // GET - BuncandoPorId e ListandoTodos
+
+
+
     [Produces("application/json")]
     [Route("api/[controller]")] // - URL: api/Estilos --
     [ApiController]
     public class EstilosController : ControllerBase // - ControllerBase é um resumo do Controller
     {
+
         List<EstiloDomain> estilos = new List<EstiloDomain>()
-            {
+        {
                 new EstiloDomain { IdEstilo = 1, Nome = "Rock" },
                 new EstiloDomain { IdEstilo = 2, Nome = "Rap" },
                 new EstiloDomain { IdEstilo = 3, Nome = "MPB" }
+        };
 
-            };
-
-        [HttpGet("{id}")]
-        public IActionResult BuscarPorId(int id)
-        {
-            EstiloDomain Estilo = estilos.Find(x => x.IdEstilo == id);
-            if (Estilo == null)
-            {
-                return NotFound();
-            }
-            return Ok(Estilo); // Retorna o Estilo, não uma mensagem "OK" :)
-        }
+     
 
         EstiloRepository EstiloRepository = new EstiloRepository();
         [HttpGet]
@@ -40,20 +36,38 @@ namespace Senai.Sstop.WebApi.Controllers
             return EstiloRepository.Listar();
         }
 
-        
-
-
         [HttpPost]
         public IActionResult Cadastrar(EstiloDomain estiloDomain)
         {
-            estilos.Add(
-                new EstiloDomain
-                {
-                    IdEstilo = estilos.Count + 1,
-                    Nome = estiloDomain.Nome
-                }
-            );
+            EstiloRepository.Cadastrar(estiloDomain);
             return Ok(estilos);
         }
+
+
+        // o controller devera receber o id que eu quero buscar
+        // GET /api/estilos/3
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            EstiloDomain estiloDomain = EstiloRepository.BuscarPorId(id);
+            if (estiloDomain == null)
+                return NotFound();
+            return Ok(estiloDomain);
+        }
+
+        [HttpDelete("{id}")] // -- Http DELETE, apenas isso
+        public IActionResult Deletar(int id)
+        {
+            EstiloRepository.Deletar(id);
+            return Ok();
+        }
+
+        [HttpPut] // -- Http DELETE, apenas isso
+        public IActionResult Atualizar(EstiloDomain estiloDomain)
+        {
+            EstiloRepository.Atualizar(estiloDomain);
+            return Ok();
+        }
+
     }
 }
--- Sstop - API/Senai.Sstop.WebApi/Senai.Sstop.WebApi/Repository/EstiloRepository.cs
+++ Sstop - API/Senai.Sstop.WebApi/Senai.Sstop.WebApi/Repository/EstiloRepository.cs
@@ -9,36 +9,30 @@ namespace Senai.Sstop.WebApi.Repositories
 {
     public class EstiloRepository
     {
-
         // aonde que será feita essa comunicação
-        // private string StringConexao = "Data Source=.\\SqlExpress;Initial Catalog=T_SStop;User Id=sa;Pwd=132;";
-        private string StringConexao = "Data Source=localhost;Initial Catalog=T_SStop;Integrated Security=true;";
-
+        private string StringConexao = "Data Source=.\\SqlExpress;Initial Catalog=T_SStop;User Id=sa;Pwd=132;";
         public List<EstiloDomain> Listar()
         {
             List<EstiloDomain> estilos = new List<EstiloDomain>();
-
-            // chamar o banco - declaro passando a string de conexão
+            // chamar o banco passando a string de conexão
             using (SqlConnection con = new SqlConnection(StringConexao))
             {
-                // nossa query a ser executada
-                string Query = "SELECT IdEstiloMusical, Nome FROM EstilosMusicais";
-                // abrir a conexao
-                con.Open();
-
-                // declaro para percorrer a lista
+                // código dB
+                string Query = "SELECT IdEstilosMusical, Nome FROM EstilosMusicas"; 
+                con.Open();        // abrir a conexao
                 SqlDataReader sdr;
+
                 // comando a ser executado em qual conexao
                 using (SqlCommand cmd = new SqlCommand(Query, con))
                 {
-                    // pegar os valores da tabela do banco e armazenar dentro da aplicacao do backend
+                    // pegar os valores da tabela do banco e armazenar dentro do SDR do backend
                     sdr = cmd.ExecuteReader();
 
                     while(sdr.Read())
                     {
                         EstiloDomain estilo = new EstiloDomain
                         {
-                            IdEstilo = Convert.ToInt32(sdr["IdEstiloMusical"]),
+                            IdEstilo = Convert.ToInt32(sdr["IdEstilosMusical"]),
                             Nome = sdr["Nome"].ToString()
                         };
                         estilos.Add(estilo);
@@ -48,8 +42,91 @@ namespace Senai.Sstop.WebApi.Repositories
             }
             // executar o select
             // retornar as informacoes
-
             return estilos;
         }
+
+          // criar um método para que eu acesse o banco de dados e busque o estilo musical aonde o id seja igual ao que eu quero pq eu mando
+        public EstiloDomain BuscarPorId(int id)
+        {
+            string Query = "SELECT IdEstilosMusical, Nome FROM EstilosMusicas WHERE IdEstilosMusical = @IdEstilosMusical";
+            // abrir a conexao
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                con.Open();
+                SqlDataReader sdr;
+
+                using (SqlCommand cmd = new SqlCommand(Query, con))
+                {
+                    cmd.Parameters.AddWithValue("@IdEstilosMusical", id);
+                    sdr = cmd.ExecuteReader();
+
+                    if (sdr.HasRows)
+                    {
+                        // ler o que tem no sdr
+                        while(sdr.Read())
+                        {
+                            EstiloDomain estilo = new EstiloDomain
+                            {
+                                IdEstilo = Convert.ToInt32(sdr["IdEstilosMusical"]),
+                                Nome = sdr["Nome"].ToString()
+                            };
+                            return estilo;
+                        }
+                    }
+                    return null;
+                    // retornar
+                }
+            }
+        }
+
+        public void Cadastrar(EstiloDomain estiloDomain)
+        {
+            string Query = "INSERT INTO EstilosMusicas (Nome) VALUES (@Nome)";
+
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                SqlCommand cmd = new SqlCommand(Query, con);
+                cmd.Parameters.AddWithValue("@Nome", estiloDomain.Nome);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Deletar (int id){
+            string Query = "DELETE FROM EstilosMusicas WHERE IdEstilosMusical = @Id";
+
+            using(SqlConnection con = new SqlConnection(StringConexao))
+            { 
+                 using (SqlCommand cmd = new SqlCommand(Query, con)) 
+                 {
+                      cmd.Parameters.AddWithValue("@Id", id);
+                      con.Open();
+                      cmd.ExecuteNonQuery();
+                 }
+            }
+        }
+
+        
+        public void Atualizar( EstiloDomain estiloDomain){
+            string Query = "UPDATE EstilosMusicais SET Nome = @Nome WHERE IdEstilosMusical = @Id";
+            
+            // estabelecer conecção
+            // comando
+            // abrir conexão
+
+            using (SqlConnection con = new SqlConnection (StringConexao))
+            {
+                SqlCommand comando = new SqlCommand(Query, con);
+                comando.Parameters.AddWithValue("@Nome", estiloDomain.Nome);
+                comando.Parameters.AddWithValue("@Id", estiloDomain.IdEstilo);
+
+                con.Open();
+                comando.ExecuteNonQuery();
+            }
+        }
+
+
+
+
     }
 }
