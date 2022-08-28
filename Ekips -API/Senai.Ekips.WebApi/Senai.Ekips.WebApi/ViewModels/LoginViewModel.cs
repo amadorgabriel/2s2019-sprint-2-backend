be351b655befae63865fb06bdbb1a9765624d4da
Gabriel Rodrigues Amador <gabrielramador2014@gmail.com>
2022-08-28 13:47:59 -0300

    chore: reset repository

--- Ekips -API/Senai.Ekips.WebApi/Senai.Ekips.WebApi/ViewModels/LoginViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Senai.Ekips.WebApi.ViewModels
-{
-    public class LoginViewModel
-    {
-        public string Email { get; set; }
-        public string Senha { get; set; }
-    }
-}
--- Gufos -API/Senai.Gufos.WebApi/Senai.Gufos.WebApi/Controllers/AutorhizeAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Senai.Gufos.WebApi.Controllers
-{
-    internal class AutorhizeAttribute : Attribute
-    {
-        public string Roles { get; set; }
-    }
-}
